15508ab4aad5a141f799c69fe4bd9a2985fc5717
Gabi <gabriela.m.pinho@gmail.com>
2015-02-05 06:12:48 +0000

    bonus: modify your script so that it correctly calculates at content for sequences like TTCGNNN and tnnacgnnat

--- Exercise_changeFunction.py
+++ Exercise_changeFunction.py
@@ -4,9 +4,16 @@
 from __future__ import division
 
 def get (dna):
-    length = len(dna)
+    for base in dna:
+        if base == "N" or base == "n":
+            length = len(dna) - dna.count(base)
+#           break will send it out of this identation, to the end of the function
+            break
+        else:
+            length = len(dna)
     a = dna.upper().count("A")
     t = dna.upper().count("T")
     at_proportion = (a + t)/length
 # this is equal to "arrendondar", 3 will be the number of numbers after the comma
     return round(at_proportion, 3)
+
